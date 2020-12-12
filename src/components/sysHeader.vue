98ef35a2047e3eef21d16be9e1cc246d0791d206
update-xin <616528688@qq.com>
2020-12-12 21:12:42 +0800

    '1'

--- src/components/sysHeader.vue
+++ src/components/sysHeader.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="header">
-            <div class="logo">
-                <img src="../assets/logo.png" alt="">&nbsp;
-                <span>渡一教育</span>
-            </div>
-        </div>
+    <div class="logo">
+      <img src="../assets/logo.png" alt="" />&nbsp;
+      <span>浙江理工大学</span>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,71 +5,71 @@ import api from '../api'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  strict:true,
-  state: {
-    showModal:false,
-    activeStu:{},
-    count:1,
-    size:2,
-    list:[],
-    nowPage:1,
-    totalPage:1,
-    searchValue:''
-  },
-  mutations: {
-    setModal(state,flag){
-      state.showModal = flag;
+    strict: true,
+    state: {
+        showModal: false,
+        activeStu: {},
+        count: 1,
+        size: 10,
+        list: [],
+        nowPage: 1,
+        totalPage: 1,
+        searchValue: ''
     },
-    setActiveStu(state,stu){
-      state.activeStu = stu;
-    },
-    changeActive(state,obj){
-      Object.assign(state.activeStu,obj)
-    },
-    setList(state,res){
-      state.list = res
-    },
-    setNowPage(state,p){
-      state.nowPage = p
-    },
-    setTotalPage(state,count){
-      state.totalPage = Math.ceil(count/state.size);
-      state.count = count;
-    },
-    setSearchValue(state,val){
-      state.searchValue = val;
-    }
-    
-  },
-  actions: {
-    async findByPage({state,commit},page){
-      const  {cont,findByPage} = await api.findByPage(page,state.size) 
-      // state.list = result.findByPage
-      commit('setList',findByPage)
-      commit('setNowPage',page)
-      commit('setTotalPage',cont)
+    mutations: {
+        setModal(state, flag) {
+            state.showModal = flag;
+        },
+        setActiveStu(state, stu) {
+            state.activeStu = stu;
+        },
+        changeActive(state, obj) {
+            Object.assign(state.activeStu, obj)
+        },
+        setList(state, res) {
+            state.list = res
+        },
+        setNowPage(state, p) {
+            state.nowPage = p
+        },
+        setTotalPage(state, count) {
+            state.totalPage = Math.ceil(count / state.size);
+            state.count = count;
+        },
+        setSearchValue(state, val) {
+            state.searchValue = val;
+        }
 
     },
-    async delStu({dispatch,state},sNo){
-      await api.delStu(sNo);
-      let page = Math.ceil((state.count-1)/state.size)
-      if(state.searchValue){
-        dispatch('searchStu',page)
-      }else{
-        dispatch('findByPage',page)
-      }
+    actions: {
+        async findByPage({ state, commit }, page) {
+            const { cont, findByPage } = await api.findByPage(page, state.size)
+            // state.list = result.findByPage
+            commit('setList', findByPage)
+            commit('setNowPage', page)
+            commit('setTotalPage', cont)
+
+        },
+        async delStu({ dispatch, state }, sNo) {
+            await api.delStu(sNo);
+            let page = Math.ceil((state.count - 1) / state.size)
+            if (state.searchValue) {
+                dispatch('searchStu', page)
+            } else {
+                dispatch('findByPage', page)
+            }
+        },
+        async searchStu({ commit, state }, page) {
+            const { cont, searchList } = await api.searchStu({
+                search: state.searchValue,
+                page,
+                size: state.size
+            });
+            commit('setTotalPage', cont);
+            commit('setNowPage', page);
+            commit('setList', searchList)
+        }
     },
-    async searchStu({commit,state},page){
-      const {cont,searchList} = await api.searchStu({
-        search:state.searchValue,
-        page,
-        size:state.size
-      });
-      commit('setTotalPage',cont);
-      commit('setNowPage',page);
-      commit('setList',searchList)
+    modules: {
     }
-  },
-  modules: {
-  }
 })
